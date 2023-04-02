177559082e273b811f768e3b6d8af63153129c6f
Đỗ Vạn Lâm <85599572+lambo1207@users.noreply.github.com>
2023-04-02 16:06:57 +0700

    Create New App
    create app

---
+++ english_card/android/app/src/main/kotlin/com/example/english_card/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.english_card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ english_card/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Scaffold(
+        appBar: AppBar(
+        ),
+        body: const Text('Create New App'),
+      ),
+    );
+  }
+}
