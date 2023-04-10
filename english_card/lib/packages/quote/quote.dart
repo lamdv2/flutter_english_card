213314c6b78a8f285ba44ccdf423a00f2ebce5b3
Đỗ Vạn Lâm <85599572+lambo1207@users.noreply.github.com>
2023-04-10 17:57:19 +0700

    finish quotes

---
+++ english_card/lib/packages/quote/quote.dart
@@ -0,0 +1,38 @@
+import 'dart:math';
+
+import 'package:english_card/packages/quote/quote_model.dart';
+
+import 'quotes.dart';
+
+class Quotes {
+  static Quotes _instance = Quotes._internal();
+  static List<Quote> datas = [];
+  Quotes._internal();
+
+  factory Quotes() => _instance;
+  getAll() {
+    // datas = await compute(convert, allquotes);
+    datas = allquotes.map((e) => Quote.fromJson(e)).toList();
+  }
+
+  static List<Quote> convert(List<dynamic> quotes) {
+    return quotes.map((e) => Quote.fromJson(e)).toList();
+  }
+
+  Quote? getByWord(String word) {
+    List<Quote> quotes = datas.where((element) {
+      String content = element.getContent() ?? " ";
+      return content.contains(word);
+    }).toList();
+    Random ran = Random();
+    return quotes.isEmpty ? null : quotes[ran.nextInt(quotes.length)];
+  }
+
+  int _getRandomIndex() {
+    return new Random.secure().nextInt(allquotes.length);
+  }
+
+  Quote getRandom() {
+    return datas[_getRandomIndex()];
+  }
+}
