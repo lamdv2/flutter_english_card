02357d693bac1a4ae29712219c0145780b0c0a1f
Đỗ Vạn Lâm <85599572+lambo1207@users.noreply.github.com>
2023-04-10 11:37:13 +0700

    Update home_page.dart
    home_page

--- english_card/lib/pages/home_page.dart
+++ english_card/lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:english_card/values/app_assets.dart';
+import 'package:english_card/values/app_colors.dart';
+import 'package:english_card/values/app_styles.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,21 +13,49 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
+      backgroundColor: AppColors.secondColor,
       appBar: AppBar(
-        title: const Text("English today"),
+        backgroundColor: AppColors.secondColor,
+        title: Text(
+          "English today",
+          style:
+              AppStyles.h4.copyWith(color: AppColors.textColor, fontSize: 36),
+        ),
+        elevation: 1,
         leading: InkWell(
-          onTap: () {
-
-          },
+          onTap: () {},
           child: Image.asset(AppAssets.menu),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => {},
+        backgroundColor: AppColors.primaryColor,
         child: Image.asset(AppAssets.exchange),
       ),
-      body: Text("homepage"),
+      body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Container(
+            width: double.infinity,
+            child: Column(
+              children: [
+                Container(
+                  height: size.height*1/10,
+                  alignment: Alignment.center,
+                  child: Text('"It is amazing"', style: AppStyles.h5,),
+                ),
+                const SizedBox(height: 20),
+                Expanded(
+                  child: Column(
+                    children: const [
+                      Text('data'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
