585d20970a9bbcfd8fc88b68d99174d68f61e98d
Đỗ Vạn Lâm <85599572+lambo1207@users.noreply.github.com>
2023-04-11 22:21:16 +0700

    feat: app button

---
+++ english_card/lib/widgets/app_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../values/app_colors.dart';
+import '../values/app_styles.dart';
+
+class AppButton extends StatelessWidget {
+  final String label;
+  final VoidCallback onTap;
+
+  const AppButton({Key? key, required this.label, required this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        onTap();
+      },
+      child: Container(
+        width: double.infinity,
+        margin: const EdgeInsets.symmetric(horizontal: 16),
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 14),
+        decoration: const BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black26, offset: Offset(3, 6), blurRadius: 6)
+            ],
+            borderRadius: BorderRadius.all(Radius.circular(8))),
+        child: Text(
+          label,
+          style: AppStyles.h5.copyWith(color: AppColors.textColor),
+        ),
+      ),
+    );
+  }
+}
