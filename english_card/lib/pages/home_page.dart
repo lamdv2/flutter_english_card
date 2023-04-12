106b2b2de6b14e5532a019b0e29086230e619d3a
Đỗ Vạn Lâm <85599572+lambo1207@users.noreply.github.com>
2023-04-12 15:59:59 +0700

    finish home_page

--- english_card/lib/pages/home_page.dart
+++ english_card/lib/pages/home_page.dart
@@ -47,20 +47,21 @@ class _HomePageState extends State<HomePage> {
   getEnglishToday() {
     List<String> getList = [];
     List<int> randoms = fixedListRandom(len: 5, max: nouns.length);
-    randoms.forEach((element) {
+    for (var element in randoms) {
       getList.add(nouns[element]);
-    });
+    }
 
     words = getList.map((e) => getWords(e)).toList();
     quote = Quotes().getRandom().content;
   }
 
-  EnglishToday getWords(noun){
+  EnglishToday getWords(noun) {
     Quote? quote;
     quote = Quotes().getByWord(noun);
     return EnglishToday(
       noun: noun,
-      quote: quote?.content ?? '"Think of all the beauty still left around you and be happy"',
+      quote: quote?.content ??
+          '"Think of all the beauty still left around you and be happy"',
       id: quote?.id,
     );
   }
@@ -71,10 +72,13 @@ class _HomePageState extends State<HomePage> {
     super.initState();
   }
 
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
+      key: _scaffoldKey,
       backgroundColor: AppColors.secondColor,
       appBar: AppBar(
         backgroundColor: AppColors.secondColor,
@@ -85,7 +89,9 @@ class _HomePageState extends State<HomePage> {
         ),
         elevation: 1,
         leading: InkWell(
-          onTap: () {},
+          onTap: () {
+            _scaffoldKey.currentState?.openDrawer();
+          },
           child: Image.asset(AppAssets.menu),
         ),
       ),
@@ -126,7 +132,7 @@ class _HomePageState extends State<HomePage> {
                   itemCount: words.length,
                   itemBuilder: (context, index) {
                     String firstLetter = words[index].noun ?? '';
-                    firstLetter = firstLetter.substring(0,1).toUpperCase();
+                    firstLetter = firstLetter.substring(0, 1).toUpperCase();
                     String leftLetter = words[index].noun ?? '';
                     leftLetter = leftLetter.substring(1, leftLetter.length);
 
@@ -190,7 +196,9 @@ class _HomePageState extends State<HomePage> {
                           Padding(
                             padding: const EdgeInsets.only(top: 48.0),
                             child: Text(
-                              words[index].quote != null ? '"${words[index].quote}"' : "Think of all the beauty still left around you and be happy",
+                              words[index].quote != null
+                                  ? '"${words[index].quote}"'
+                                  : "Think of all the beauty still left around you and be happy",
                               style: AppStyles.h4.copyWith(
                                   letterSpacing: .6,
                                   color: AppColors.textColor),
@@ -235,20 +243,20 @@ class _HomePageState extends State<HomePage> {
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 24),
                 child: AppButton(
-                    label: 'Favorites',
-                    onTap: () {
-                      print('Favorites');
-                    }),
+                  label: 'Favorites',
+                  onTap: () {},
+                ),
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 24),
                 child: AppButton(
-                    label: 'Your control',
-                    onTap: () {
-                      // Navigator.push(context,
-                      //     MaterialPageRoute(builder: (_) => ControlPage()));
-                    }),
-              )
+                  label: 'Your control',
+                  onTap: () {
+                    // Navigator.push(context,
+                    //     MaterialPageRoute(builder: (_) => ControlPage()));
+                  },
+                ),
+              ),
             ],
           ),
         ),
